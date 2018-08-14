83385d456b8d6dcfed0c9ce6b7a00af51b22734b
skeleton625 <chaos0824@naver.com>
2018-08-14 22:43:29 +0900

    2920 문제 숏코드 구현

--- baekjoon_2920/main.cpp
+++ baekjoon_2920/main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int i1[8], r;
-
+int i1;
+unsigned long r;
 int main() {
-	for (int i = 0; i < 8; i++) cin >> i1[i];
-	for (int i = 0; i < 7; i++) {
-		if (i1[i] < i1[i + 1]) r++;
-		else r--;
+	for (int i = 0; i < 8; i++) {
+		cin >> i1;
+		r = r * 10 + i1;
 	}
-	if (r == -7) cout << "descending";
-	else if (r == 7) cout << "ascending";
-	else cout << "mixed";
+	cout << (r == 12345678 ? "ascending" : (r == 87654321 ? "descending" : "mixed"));
 	return 0;
 }
